fc514af7ac0175adba60bf02262fa0505c7997aa
German Kuznetsov <KuznetsovGA@pay-trans.ru>
2022-01-03 20:21:18 +0300

    up-to-date project

--- Assets/Scripts/EquipmentSet.cs
+++ Assets/Scripts/EquipmentSet.cs
@@ -4,21 +4,70 @@
 [RequireComponent(typeof (Inventory))]
 public class EquipmentSet : MonoBehaviour
 {
-	public EquipmentSlot rightHand = new EquipmentSlot();
-	public EquipmentSlot leftHand = new EquipmentSlot();
+    public EquipmentSlot rightHand;
+	public EquipmentSlot leftHand;
 
-	public EquipmentSlot helmet = new EquipmentSlot();
-	public EquipmentSlot platebody = new EquipmentSlot();
-	public EquipmentSlot cloak = new EquipmentSlot();
-	public EquipmentSlot gloves = new EquipmentSlot();
-    public EquipmentSlot belt = new EquipmentSlot();
-    public EquipmentSlot pants = new EquipmentSlot();
-    public EquipmentSlot shoes = new EquipmentSlot();
-    public EquipmentSlot necklace = new EquipmentSlot();
-    public EquipmentSlot ring1 = new EquipmentSlot();
-    public EquipmentSlot ring2 = new EquipmentSlot();
-    public EquipmentSlot quiver = new EquipmentSlot();
+	public EquipmentSlot helmet;
+	public EquipmentSlot platebody;
+	public EquipmentSlot cloak;
+	public EquipmentSlot gloves;
+    public EquipmentSlot belt;
+    public EquipmentSlot pants;
+    public EquipmentSlot shoes;
+    public EquipmentSlot necklace;
+    public EquipmentSlot ring1;
+    public EquipmentSlot ring2;
+    public EquipmentSlot quiver;
     
+
+    void Awake()
+    {
+        rightHand = ScriptableObject.CreateInstance<EquipmentSlot>();
+        leftHand = ScriptableObject.CreateInstance<EquipmentSlot>();
+
+        helmet = ScriptableObject.CreateInstance<EquipmentSlot>();
+        platebody = ScriptableObject.CreateInstance<EquipmentSlot>();
+        cloak = ScriptableObject.CreateInstance<EquipmentSlot>();
+        gloves = ScriptableObject.CreateInstance<EquipmentSlot>();
+        belt = ScriptableObject.CreateInstance<EquipmentSlot>();
+        pants = ScriptableObject.CreateInstance<EquipmentSlot>();
+        shoes = ScriptableObject.CreateInstance<EquipmentSlot>();
+        necklace = ScriptableObject.CreateInstance<EquipmentSlot>();
+        ring1 = ScriptableObject.CreateInstance<EquipmentSlot>();
+        ring2 = ScriptableObject.CreateInstance<EquipmentSlot>();
+        quiver = ScriptableObject.CreateInstance<EquipmentSlot>();
+
+
+        rightHand.SetType(Item.Type.WEAPON);
+        leftHand.SetType(Item.Type.WEAPON | Item.Type.SHIELD);
+
+        helmet.SetType(Item.Type.HELMET);
+        platebody.SetType(Item.Type.PLATEBODY);
+        cloak.SetType(Item.Type.CLOAK);
+        gloves.SetType(Item.Type.GLOVES);
+        belt.SetType(Item.Type.BELT);
+        pants.SetType(Item.Type.PANTS);
+        shoes.SetType(Item.Type.SHOES);
+        necklace.SetType(Item.Type.NECKLACE);
+        ring1.SetType(Item.Type.RING);
+        ring2.SetType(Item.Type.RING);
+        quiver.SetType(Item.Type.QUIVER);
+
+        rightHand.OnExchange += CalcEquipment;
+        leftHand.OnExchange += CalcEquipment;
+
+        helmet.OnExchange += CalcEquipment;
+        platebody.OnExchange += CalcEquipment;
+        cloak.OnExchange += CalcEquipment;
+        gloves.OnExchange += CalcEquipment;
+        belt.OnExchange += CalcEquipment;
+        pants.OnExchange += CalcEquipment;
+        shoes.OnExchange += CalcEquipment;
+        necklace.OnExchange += CalcEquipment;
+        ring1.OnExchange += CalcEquipment;
+        ring2.OnExchange += CalcEquipment;
+        quiver.OnExchange += CalcEquipment;
+    }
 
 
     public void CalcEquipment()
@@ -115,39 +164,5 @@ public class EquipmentSet : MonoBehaviour
 
         Stats stats = GetComponent<CommonCharacterController>().Parameters;
         stats.EquipmentParameters = param;
-    }
-
-
-    void Awake()
-    {
-        rightHand.SetType(Item.Type.WEAPON);
-        leftHand.SetType(Item.Type.WEAPON | Item.Type.SHIELD);
-
-        helmet.SetType(Item.Type.HELMET);
-        platebody.SetType(Item.Type.PLATEBODY);
-        cloak.SetType(Item.Type.CLOAK);
-        gloves.SetType(Item.Type.GLOVES);
-        belt.SetType(Item.Type.BELT);
-        pants.SetType(Item.Type.PANTS);
-        shoes.SetType(Item.Type.SHOES);
-        necklace.SetType(Item.Type.NECKLACE);
-        ring1.SetType(Item.Type.RING);
-        ring2.SetType(Item.Type.RING);
-        quiver.SetType(Item.Type.QUIVER);
-
-        rightHand.OnExchange += CalcEquipment;
-		leftHand.OnExchange += CalcEquipment;
-
-		helmet.OnExchange += CalcEquipment;
-		platebody.OnExchange += CalcEquipment;
-		cloak.OnExchange += CalcEquipment;
-		gloves.OnExchange += CalcEquipment;
-		belt.OnExchange += CalcEquipment;
-		pants.OnExchange += CalcEquipment;
-		shoes.OnExchange += CalcEquipment;
-		necklace.OnExchange += CalcEquipment;
-		ring1.OnExchange += CalcEquipment;
-		ring2.OnExchange += CalcEquipment;
-		quiver.OnExchange += CalcEquipment;
     }
 }
--- Assets/Scripts/Items/EquipmentSlot.cs
+++ Assets/Scripts/Items/EquipmentSlot.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EquipmentSlot
+public class EquipmentSlot : ScriptableObject
 {
 	private Equipment equipment;
     private uint type;
